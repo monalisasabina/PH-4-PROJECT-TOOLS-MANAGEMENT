6783756bc8934c6bc5e355ad7e9b63f3869e615c
Harmony <harmonymukolwe@gmail.com>
2024-10-17 11:33:10 +0300

    Creates tool card

--- client/src/components/ToolCard.jsx
+++ client/src/components/ToolCard.jsx
@@ -1,18 +1,29 @@
-function ToolCard({name, image, totalNumber,availableTools,brand, purchaseDate}){
+import "../Pages/tools.css";
 
-
-    return(
-        <>
-           
-           <h3> {name}</h3>
-           <img src={image}/>
-           <p>Brand:{brand}</p>
-           <p>Total Number:{totalNumber}</p>
-           <p>Available Tools:{availableTools}</p>
-           <p>Purchase Date: {purchaseDate}</p>
-        </>
-    )
+function ToolCard({
+  name,
+  image,
+  totalNumber,
+  availableTools,
+  brand,
+  purchaseDate,
+}) {
+  return (
+    <div className="tool_card">
+      <img src={image} className="tool_card-img" />
+      <h3 className="tool_card-title"> {name}</h3>
+      <div className="tool_card-content">
+        <p>Brand:{brand}</p>
+        <p>Total Number:{totalNumber}</p>
+        <p>Available Tools:{availableTools}</p>
+        <p>Purchase Date: {purchaseDate}</p>
+        <div className="tool_card-btns">
+          <div className="tool_card-btn minus">-</div>
+          <div className="tool_card-btn plus">+</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ToolCard
-
+export default ToolCard;
